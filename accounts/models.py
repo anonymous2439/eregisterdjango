f88bab174b5e8debfece0da66829fa3a4fdf0e05
anonymous2439 <mranonymous2439@gmail.com>
2021-04-09 19:15:06 +0800

    changes

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser, PermissionsMixin
 )
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image, ImageDraw
 
 
 class UserManager(BaseUserManager):
@@ -39,7 +43,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=30, unique=True)
     contact_no = models.CharField(max_length=20, blank=True)
-    role = models.ForeignKey(UserRole, on_delete=models.CASCADE, null=True, default=None)
+    role = models.ForeignKey(UserRole, on_delete=models.CASCADE, null=True, default=None, blank=True)
     qr_code = models.ImageField(upload_to='qr_code', null=True)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
@@ -47,3 +51,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    def save(self, *args, **kwargs):
+        qrcode_img = qrcode.make(self.email)
+        canvas = Image.new('RGB', (qrcode_img.pixel_size, qrcode_img.pixel_size), 'white')
+        draw = ImageDraw.Draw(canvas)
+        canvas.paste(qrcode_img)
+        file_name = f'qr_code-{self.email}.png'
+        buffer = BytesIO()
+        canvas.save(buffer, 'PNG')
+        self.qr_code.save(file_name, File(buffer), save=False)
+        canvas.close()
+        super().save(*args, **kwargs)
