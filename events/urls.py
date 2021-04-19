04ed3d06eaa1e479b1a3b67c612079c4b9646d6e
anonymous2439 <mranonymous2439@gmail.com>
2021-04-19 10:17:19 +0800

    changes

--- events/urls.py
+++ events/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='events-home'),
+    path('add/', views.AddEvent.as_view(), name='events-add'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,8 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
 from accounts.models import User
-from events.models import Event
+from events.models import Event, EventType
 
 
 class Home(View):
@@ -13,3 +14,22 @@ class Home(View):
             'events': Event.objects.all(),
         }
         return render(request, self.name, context)
+
+
+class AddEvent(View):
+    name = 'events/add.html'
+
+    def get(self, request):
+        context = {
+            'eventTypes': EventType.objects.all(),
+        }
+        return render(request, self.name, context)
+
+    def post(self, request):
+        name = request.POST.get('name', '')
+        venue = request.POST.get('venue', '')
+        event_type = int(request.POST.get('event-type', '0'))
+        organizer = request.user
+        print(f'{name} / {venue} / {event_type}')
+        event = Event(name=name, venue=venue, event_type=EventType.objects.get(pk=event_type), organizer=organizer).save()
+        return HttpResponseRedirect(self.request.path_info)
