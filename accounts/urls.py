05af43a5e302a1e82bc3b257fa2edd4f703ec035
anonymous2439 <mranonymous2439@gmail.com>
2021-04-24 17:31:00 +0800

    changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.Profile.as_view(), name='accounts-profile'),
+    path('login/', views.Login.as_view(), name='accounts-login'),
+    path('manage/', views.ManageAccounts.as_view(), name='accounts-manage'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.views import View
 
 from accounts.models import User
@@ -11,5 +13,37 @@ class Profile(View):
         user = User.objects.get(pk=1)
         context = {
             'user': user,
+        }
+        return render(request, self.name, context)
+
+
+class Login(View):
+    name = 'accounts/login.html'
+
+    def get(self, request):
+        return render(request, self.name)
+
+    def post(self, request):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(email=email, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, f'Welcome {user.email}')
+            if request.POST.get('next'):
+                return redirect(request.POST.get('next'))
+            return redirect('/events/')
+        else:
+            messages.error(request, 'Invalid Username/Password')
+        return render(request, self.name)
+
+
+class ManageAccounts(View):
+    name = 'accounts/manage.html'
+
+    def get(self, request):
+        users = User.objects.all()
+        context = {
+            'users': users,
         }
         return render(request, self.name, context)
