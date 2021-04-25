d2ec047c46c3d62d299d97c15874e8bf4de7daf4
anonymous2439 <mranonymous2439@gmail.com>
2021-04-25 17:38:53 +0800

    changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from django.views import View
 
-from accounts.models import User
+from accounts.models import User, UserRole
 
 
 class Profile(View):
@@ -46,10 +47,35 @@ class ManageAccounts(View):
 
     def get(self, request):
         users = User.objects.all()
+        user_roles = UserRole.objects.all()
         context = {
             'users': users,
+            'user_roles': user_roles,
         }
         return render(request, self.name, context)
+
+    def post(self, request):
+        if request.is_ajax():
+            # deleting user
+            if request.POST.get('delete') == '1':
+                print(request.POST.get('user_id'))
+                user = User.objects.get(pk=int(request.POST.get('user_id')))
+                user.delete()
+                messages.success(request, 'A user has been successfully deleted!')
+                return JsonResponse({'success': 1})
+            return JsonResponse({'success': 0})
+        else:
+            user = User(
+                first_name=request.POST.get('fname'),
+                middle_name=request.POST.get('mname'),
+                last_name=request.POST.get('lname'),
+                email=request.POST.get('email'),
+                contact_no=request.POST.get('contact-no'),
+                role=UserRole.objects.get(pk=int(request.POST.get('role'))),
+            )
+            user.set_password('password')
+            user.save()
+            return redirect('accounts-manage')
 
 
 def logout(request):
